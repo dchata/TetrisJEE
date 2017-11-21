73fcc6df00c74f29b501929c288107e3315f29e1
chata <chata@192.168.200.99>
2017-11-21 17:06:49 +0100

    init model classes

--- TetrisTP/src/fr/iia/tetris/model/Tetriminos.java
+++ TetrisTP/src/fr/iia/tetris/model/Tetriminos.java
@@ -1,5 +1,25 @@
 package fr.iia.tetris.model;
 
 public class Tetriminos {
-	//TODO: créer tetriminos
+	private String name;
+	private String color;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public Tetriminos() {}
+	
+	public Tetriminos(String nom, String couleur) {
+		name = nom;
+		color = couleur;
+	}
 }
--- TetrisTP/src/fr/iia/tetris/model/Utilisateurs.java
+++ TetrisTP/src/fr/iia/tetris/model/Utilisateurs.java
@@ -1,7 +1,25 @@
 package fr.iia.tetris.model;
 
 public class Utilisateurs {
-
-	//TODO: créer des utilisateurs
+	private String username;
+	private String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
+	public Utilisateurs(){}
+	
+	public Utilisateurs(String nom, String mdp) {
+		username = nom;
+		password = mdp;
+	}
 }
